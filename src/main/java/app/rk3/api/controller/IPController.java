3379b6b002262c3580fa819ce5bfaa2bfb66cd5c
Ricky Li <cnrickylee@gmail.com>
2018-10-18 23:06:07 +0800

    upd

--- src/main/java/app/rk3/api/controller/IPController.java
+++ src/main/java/app/rk3/api/controller/IPController.java
@@ -13,7 +13,16 @@ public class IPController {
 
     @RequestMapping(value = "/ip", method = RequestMethod.GET)
     public Map getRequestIp(HttpServletRequest request) {
-        String ip = request.getRemoteAddr();
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("ip", ip);
         return responseMap;
