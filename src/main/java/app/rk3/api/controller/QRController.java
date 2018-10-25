001b1f9be3efb36dc0b187d6408b9d5909c6a026
xyzrlee <cnrickylee@gmail.com>
2018-10-25 10:57:09 +0800

    fix

--- src/main/java/app/rk3/api/controller/QRController.java
+++ src/main/java/app/rk3/api/controller/QRController.java
@@ -4,7 +4,6 @@ import app.rk3.api.exception.UnsupportedOperationException;
 import app.rk3.api.util.FileUtils;
 import app.rk3.api.util.QRCodeUtils;
 import com.google.zxing.Result;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,22 +19,10 @@ import java.util.Map;
 @RestController
 public class QRController {
 
-    private static final String METHOD_DECODE = "d";
-    private static final String METHOD_ENCODE = "e";
     private static final String TEMP_FILE_PREFIX = "api-qr-";
     private static final String TEMP_FILE_SUFFIX = "";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @PostMapping("/qr")
-    public Map root(HttpServletRequest request) {
-        String m = request.getParameter("m");
-        if (null == m)
-            throw new UnsupportedOperationException("parameter m is missing");
-        if (StringUtils.equals(m, METHOD_DECODE))
-            return this.decode(request);
-        throw new UnsupportedOperationException(String.format("unsupported operation: m=%s", m));
-    }
 
     @PostMapping("/qr/decode")
     private Map decode(HttpServletRequest request) {
