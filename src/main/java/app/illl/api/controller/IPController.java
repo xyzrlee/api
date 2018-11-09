b637d5c476f5bf5d0df26629b5d5aa723bff6df7
Ricky Li <cnrickylee@gmail.com>
2018-11-09 20:09:04 +0800

    test

--- src/main/java/app/illl/api/controller/IPController.java
+++ src/main/java/app/illl/api/controller/IPController.java
@@ -1,6 +1,8 @@
 package app.illl.api.controller;
 
 import app.illl.api.struct.io.ip.IPResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Api(value = "IP", description = "获得公网IP")
 @RestController
 public class IPController {
 
     private static final String IP_UNKNOWN = "unknown";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @ApiOperation(value = "获取请求IP")
     @GetMapping(path = "/ip")
     public IPResponse getRequestIp(HttpServletRequest httpServletRequest) {
-        logger.info("getRequestIp");
+        logger.info("{}", getClass().getPackage().getImplementationVersion());
         String ip = httpServletRequest.getHeader("x-forwarded-for");
         if (ip == null || ip.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ip)) {
             ip = httpServletRequest.getHeader("Proxy-Client-IP");
