5168ed5fc69a7b16ad7001903592935940c1b7a5
xyzrlee <cnrickylee@gmail.com>
2019-01-15 11:11:41 +0800

    upd

--- src/main/java/app/illl/api/controller/IP.java
+++ src/main/java/app/illl/api/controller/IP.java
@@ -1,7 +1,10 @@
 package app.illl.api.controller;
 
 import app.illl.api.struct.io.ip.IPResponse;
+import com.google.common.net.HttpHeaders;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class IP {
 
     private static final String IP_UNKNOWN = "unknown";
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping(path = "/ip")
     public IPResponse getRequestIp(HttpServletRequest httpServletRequest) {
-        String ip = httpServletRequest.getHeader("X-Forwarded-For");
+        String ip = httpServletRequest.getHeader(HttpHeaders.X_FORWARDED_FOR);
+        logger.info("{}: {}", HttpHeaders.X_FORWARDED_FOR, ip);
         if (isValidIP(ip)) {
             String[] ips = StringUtils.split(ip, ',');
             ip = ips[0];
