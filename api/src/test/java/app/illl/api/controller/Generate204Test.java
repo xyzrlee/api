a329a563a85754e5a317ac947784947f37420893
xyzrlee <cnrickylee@gmail.com>
2019-05-09 18:26:20 +0800

    fix

--- api/src/test/java/app/illl/api/controller/Generate204Test.java
+++ api/src/test/java/app/illl/api/controller/Generate204Test.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
--- api/src/test/java/app/illl/api/controller/IPTest.java
+++ api/src/test/java/app/illl/api/controller/IPTest.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
